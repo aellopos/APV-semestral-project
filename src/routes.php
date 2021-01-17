b0de6dbcb675e2b01f4c138cea4487cf424d34c7
Michal Kučera <39790985+aellopos@users.noreply.github.com>
2021-01-17 18:08:17 +0100

    Contact page

--- src/routes.php
+++ src/routes.php
@@ -25,22 +25,22 @@ $app->post('/test', function (Request $request, Response $response, $args) {
 /* Seznam vsech osob */
 
 $app->get('/persons', function (Request $request, Response $response, $args) {
-	$stmt = $this->db->query('SELECT * FROM person ORDER BY first_name'); 
+    $stmt = $this->db->query('SELECT * FROM person ORDER BY first_name');
 
-	$tplVars['persons_list'] = $stmt->fetchall(); 
-	return $this->view->render($response, 'persons.latte', $tplVars);
+    $tplVars['persons_list'] = $stmt->fetchall();
+    return $this->view->render($response, 'persons.latte', $tplVars);
 })->setname('persons');
 
 /* Vyhledavani */
 $app->get('/search', function (Request $request, Response $response, $args) {
     /*parametry URL*/
     $queryParams = $request->getQueryParams();
-    if(!empty($queryParams)) {
+    if (!empty($queryParams)) {
         $stmt = $this->db->prepare('SELECT * FROM person WHERE lower(first_name) = lower(:fname) OR lower(last_name) = lower(:lname)');
         $stmt->bindParam(':fname', $queryParams['q']);
         $stmt->bindParam(':lname', $queryParams['q']);
         $stmt->execute();
-        $tplVars['persons_list'] = $stmt->fetchall(); 
+        $tplVars['persons_list'] = $stmt->fetchall();
 
         return $this->view->render($response, 'persons.latte', $tplVars);
     }
@@ -73,7 +73,7 @@ $app->get('/person/update', function (Request $request, Response $response, $arg
         $stmt->bindValue(':id_person', $params['id_person']);
         $stmt->execute();
         $tplVars['formData'] = $stmt->fetch();
-        if(empty($tplVars['formData'])) {
+        if (empty($tplVars['formData'])) {
             exit('person not found');
         } else {
             return $this->view->render($response, 'updatePerson.latte', $tplVars);
@@ -85,11 +85,11 @@ $app->post('/person/update', function (Request $request, Response $response, $ar
     $id_person = $request->getQueryParam('id_person');
     $formData = $request->getParsedBody();
     $tplVars = [];
-    if ( empty($formData['first_name']) || empty($formData['last_name']) || empty($formData['nickname']) ) {
+    if (empty($formData['first_name']) || empty($formData['last_name']) || empty($formData['nickname'])) {
         $tplVars['message'] = 'Please fill required fields';
     } else {
         try {
-            if(!empty($formData['street_name']) || !empty($formData['street_number']) || !empty($formData['zip']) || !empty($formData['zip'])) {
+            if (!empty($formData['street_name']) || !empty($formData['street_number']) || !empty($formData['zip']) || !empty($formData['zip'])) {
 
                 $stmt = $this->db->prepare("SELECT id_location FROM person WHERE id_person = :id_person");
                 $stmt->bindValue(':id_person', $id_person);
@@ -97,7 +97,7 @@ $app->post('/person/update', function (Request $request, Response $response, $ar
 
                 $id_location = $stmt->fetch()['id_location'];
 
-                if($id_location) {
+                if ($id_location) {
                     #ma adresu
                     editLocation($this, $id_location, $formData);
                 } else {
@@ -120,7 +120,7 @@ $app->post('/person/update', function (Request $request, Response $response, $ar
             $stmt->bindValue(':first_name', $formData['first_name']);
             $stmt->bindValue(':last_name', $formData['last_name']);
             $stmt->bindValue(':id_location', $id_location ? $id_location : null);
-            $stmt->bindValue(':gender', empty($formData['gender']) ? null : $formData['gender'] );
+            $stmt->bindValue(':gender', empty($formData['gender']) ? null : $formData['gender']);
             $stmt->bindValue(':birth_day', empty($formData['birth_day']) ? null : $formData['birth_day']);
             $stmt->bindValue(':height', empty($formData['height']) ? null : $formData['height']);
             $stmt->bindValue(':id_person', $id_person);
@@ -128,7 +128,7 @@ $app->post('/person/update', function (Request $request, Response $response, $ar
 
             $tplVars['message'] = 'Person successfully updated';
         } catch (PDOexception $e) {
-            $tplVars['message'] = 'Error occured, sorry jako';
+            $tplVars['message'] = 'Error occured';
             $this->logger->error($e->getMessage());
         }
     }
@@ -148,7 +148,7 @@ $app->post('/person', function (Request $request, Response $response, $args) {
         try {
             $this->db->beginTransaction();
 
-            if(!empty($formData['street_name']) || !empty($formData['street_number']) || !empty($formData['zip']) || !empty($formData['zip'])) {
+            if (!empty($formData['street_name']) || !empty($formData['street_number']) || !empty($formData['zip']) || !empty($formData['zip'])) {
                 #nema adresu
                 $id_location = newLocation($this, $formData);
             }
@@ -182,7 +182,7 @@ $app->post('/person', function (Request $request, Response $response, $args) {
 
 /* mazani osob */
 
-$app->post('/persons/delete', function (Request $request, Response $response, $args ) {
+$app->post('/persons/delete', function (Request $request, Response $response, $args) {
     $id_person = $request->getQueryParam('id_person');
     if (!empty($id_person)) {
         try {
@@ -205,4 +205,133 @@ $app->post('/persons/delete', function (Request $request, Response $response, $a
 
 
 
-# OTHER
+/* CONTACT */
+
+
+/* Zobrazit */
+$app->get('/contact', function (Request $request, Response $response, $args) {
+    $id_person = $request->getQueryParam('id_person');
+
+    if (!empty($id_person)) {
+        $stmt = $this->db->prepare('SELECT * FROM person INNER JOIN contact USING (id_person) INNER JOIN contact_type USING (id_contact_type) WHERE id_person = :id_person');
+        $stmt->bindValue(':id_person', $id_person);
+        $stmt->execute();
+        $tplVars['contactData'] = $stmt->fetchall();
+        $tplVars['person'] = ["id_person" => $id_person];
+        if (empty($tplVars['contactData'])) {
+            exit('person not found');
+        } else {
+            return $this->view->render($response, 'profileContact.latte', $tplVars);
+        }
+    }
+})->setname('viewContact');
+
+/* Pridat */
+
+/* render formulare */
+$app->get('/contact/new', function (Request $request, Response $response, $args) {
+    $id_person = $request->getQueryParam('id_person');
+
+    $tplVars['contactData'] = [
+        'contact' => '',
+        'name' => '',
+        'id_person' => $id_person
+    ];
+
+    return $this->view->render($response, 'newContact.latte', $tplVars);
+})->setname('newContact');
+
+/* zpracovani */
+$app->post('/contact/new', function (Request $request, Response $response, $args) {
+    $formData = $request->getParsedBody();
+    $tplVars = ['formData' => ''];
+
+    if (empty($formData['contact_name'])) {
+        $tplVars['message'] = 'Please field required fields';
+    } else {
+        try {
+            $stmt = $this->db->prepare("INSERT INTO contact(id_person, id_contact_type, contact) VALUES (:id_person, :id_contact_type, :contact)");
+
+            $stmt->bindValue(':id_person', $formData['id_person']);
+            $stmt->bindValue(':id_contact_type', $formData['contact_type']);
+            $stmt->bindValue(':contact', $formData['contact_name']);
+            $stmt->execute();
+            $tplVars['message'] = 'Contact successfully added';
+        } catch (PDOException $e) {
+            $tplVars['message'] = 'Error occured, working on it';
+            /*log erroru*/
+            $this->logger->error($e->getMessage());
+
+            /*zachovani hodnot formu*/
+            $tplVars['formData'] = $formData;
+        }
+    }
+
+    return $this->view->render($response, 'newContact.latte', $tplVars);
+});
+
+
+/* Upravit */
+
+$app->get('/contact/update', function (Request $request, Response $response, $args) {
+    $params = $request->getqueryParams();
+
+    if (!empty($params['id_contact'])) {
+        $stmt = $this->db->prepare('SELECT * FROM contact INNER JOIN contact_type USING (id_contact_type) WHERE id_contact = :id_contact');
+        $stmt->bindValue(':id_contact', $params['id_contact']);
+        $stmt->execute();
+        $tplVars['contactData'] = $stmt->fetch();
+        if (empty($tplVars['contactData'])) {
+            exit('contact not found');
+        } else {
+            return $this->view->render($response, 'updateContact.latte', $tplVars);
+        }
+    }
+})->setname('updateContact');
+
+$app->post('/contact/update', function (Request $request, Response $response, $args) {
+    $id_contact = $request->getQueryParam('id_contact');
+    $formData = $request->getParsedBody();
+    $tplVars = [];
+    if (empty($formData['contact_name'])) {
+        $tplVars['message'] = 'Please fill required fields';
+    } else {
+        try {
+            $stmt = $this->db->prepare("UPDATE contact SET 
+                                                contact = :contact,  
+                                                id_contact_type = :id_contact_type
+                                        WHERE id_contact = :id_contact");
+            $stmt->bindValue(':contact', $formData['contact_name']);
+            $stmt->bindValue(':id_contact_type', $formData['contact_type']);
+            $stmt->bindValue(':id_contact', $id_contact);
+            $stmt->execute();
+
+            $tplVars['message'] = 'Contact successfully updated';
+        } catch (PDOexception $e) {
+            $tplVars['message'] = 'Error occured';
+            $this->logger->error($e->getMessage());
+        }
+    }
+    $tplVars['formData'] = $formData;
+    return $this->view->render($response, 'updateContact.latte', $tplVars);
+});
+
+/* Smazat */
+
+$app->post('/contact/delete', function (Request $request, Response $response, $args) {
+    $id_contact = $request->getQueryParam('id_contact');
+    if (!empty($id_contact)) {
+        try {
+            $stmt = $this->db->prepare('DELETE FROM contact WHERE id_contact = :id_contact');
+            $stmt->bindValue(':id_contact', $id_contact);
+            $stmt->execute();
+        } catch (PDOException $e) {
+            $this->logger->error($e->getMessage());
+            exit('error occured');
+        }
+    } else {
+        exit('id_person is missing');
+    }
+
+    return $response->withHeader('Location', $this->router->pathFor('persons'));
+})->setname('contact_delete');
